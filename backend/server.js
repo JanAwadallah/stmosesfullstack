3a4914be294fccc7a16f1c97df2665537498f01a
JanAwadallah <jan.awadallah@gmail.com>
2022-07-02 02:47:32 +1000

    commit

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,8 @@ if (process.env.NODE_ENV === "production") {
 }
 app.use(errorHandler);
 
-app.listen(process.env.PORT || 5000, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
   console.log(
     `Server listening to port ${PORT} in ${process.env.NODE_ENV} mode `
   );
